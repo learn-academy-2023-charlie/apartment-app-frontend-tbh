4399b5c072914455344439dbc1422e165d66d4fb
bkre12345 <belvira@terpmail.umd.edu>
2023-06-16 01:20:20 -0400

    added index and homepage styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { Button } from 'reactstrap'
+import apt1 from '../public/apt1.png'
 const Home = () => {
   return (
     <>
-      <h3> Find your new home </h3>
-        <body class="homeimg" >
-          <img alt="apartment image" src="../apt1.png"></img>
-        </body> 
+      <div class="homeimg">
+        <div className='home-header'> Find your new home today</div>
+        <img src={apt1} alt="apt 1"/>
+        <br />
+        <Link to="/index">
+          <Button className="home-btn">
+            Check out all of our available listings here
+          </Button>
+        </Link>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home
